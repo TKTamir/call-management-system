7accdb4d032562ba4a9711329bb9f24dc2f13174
tamir <tamirkhy@gmail.com>
2025-05-22 22:20:44 +0300

    Adjust task model

--- apps/server/src/models/task.ts
+++ apps/server/src/models/task.ts
@@ -1,7 +1,5 @@
 import { Model, DataTypes } from "sequelize";
 import { sequelize } from "@config/database";
-import Call from "@models/call";
-import Tag from "@models/tag";
 import {
   type TaskAttributes,
   type TaskInput,
@@ -10,21 +8,10 @@ import {
 class Task extends Model<TaskAttributes, TaskInput> implements TaskAttributes {
   declare id: number;
   declare name: string;
+  declare isSuggested: boolean;
 
   declare readonly createdAt: Date;
   declare readonly updatedAt: Date;
-
-  // Association with calls
-  declare getCalls: () => Promise<Call[]>;
-  declare setCalls: (calls: Call[]) => Promise<void>;
-  declare addCall: (call: Call) => Promise<void>;
-  declare removeCall: (call: Call) => Promise<void>;
-
-  // Association with tags
-  declare getRelatedTags: () => Promise<Tag[]>;
-  declare setRelatedTags: (tags: Tag[]) => Promise<void>;
-  declare addRelatedTag: (tag: Tag) => Promise<void>;
-  declare removeRelatedTag: (tag: Tag) => Promise<void>;
 }
 
 Task.init(
@@ -37,6 +24,10 @@ Task.init(
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    isSuggested: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
   },
   {
