92e04b1368c7fc9355bf7c6b288033a0edd99853
tamir <tamirkhy@gmail.com>
2025-05-23 20:30:48 +0300

    Create protected route component

---
+++ apps/client/src/components/AppContent/AppContent.tsx
@@ -0,0 +1,30 @@
+//TODO: Install React-router and implement Routes
+const AppContent: React.FC = () => {
+  return (
+    <></>
+    // <Routes>
+    //   <Route path="/login" element={<Home />} /> Home Includes Login Modal
+    //
+    //   <Route
+    //     path="/admin"
+    //     element={
+    //       <ProtectedRoute requiredRole="admin">
+    //         <AdminDashboard /> Admin Dashboard Includes toggle for switching between tags and suggested tasks views, and a toggle for switching to  calls view
+    //       </ProtectedRoute>
+    //     }
+    //   />
+    //   <Route
+    //     path="/user"
+    //     element={
+    //       <ProtectedRoute requiredRole="user">
+    //         <UserDashboard /> // User Dashboard includes calls view
+    //       </ProtectedRoute>
+    //     }
+    //   />
+    //   <Route path="/unauthorized" element={<div>Unauthorized Access</div>} />
+    //   <Route path="/" element={<Navigate to="/login" replace />} />
+    // </Routes>
+  );
+};
+
+export default AppContent;
