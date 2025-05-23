9db93df237ee12c418307f54b5bdc1542a980c3f
tamir <tamirkhy@gmail.com>
2025-05-23 13:32:12 +0300

    Create homepage component

---
+++ apps/client/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <div className="text-center p-6 border rounded-lg bg-white">
+        <h2 className="text-xl font-semibold mb-4">Welcome to Cytactic</h2>
+        <p>Please sign in to access the application</p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
