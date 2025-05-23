db8a9664f4acaace2febc2797dfc66f8a5acf4e5
tamir <tamirkhy@gmail.com>
2025-05-23 23:41:15 +0300

    Reorganize app.tsx imports

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
 import { AppProvider } from "./providers/AppProvider";
 import AppContent from "./components/AppContent/AppContent";
-import { ToastContainer } from "react-toastify";
 
 const App: React.FC = () => {
   return (
