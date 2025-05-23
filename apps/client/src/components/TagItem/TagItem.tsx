20a7a9a9efc09f45f1e684653301581337a54573
tamir <tamirkhy@gmail.com>
2025-05-24 01:18:41 +0300

    Create tag item component

---
+++ apps/client/src/components/TagItem/TagItem.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+interface TagItemProps {
+  name: string;
+  className?: string;
+  highlightIcon?: boolean;
+}
+
+const TagItem: React.FC<TagItemProps> = ({
+  name,
+  className,
+  highlightIcon = false,
+}) => {
+  return (
+    <div className={`flex items-center justify-between ${className}`}>
+      <span>{name}</span>
+      {highlightIcon && <span className="ml-2">+️</span>}
+    </div>
+  );
+};
+
+export default TagItem;
