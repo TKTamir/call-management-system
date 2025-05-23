b70a999fd42f9a13c758897bf72fd7dd732cbf4b
tamir <tamirkhy@gmail.com>
2025-05-23 12:19:00 +0300

    Create useSocket hook

---
+++ apps/client/src/hooks/useSocket.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { socket } from "../sockets/socket";
+
+//TODO: Create Path Alias for Hooks
+
+export const useSocket = () => {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    const handleConnect = () => setIsConnected(true);
+    const handleDisconnect = () => setIsConnected(false);
+
+    socket.on("connect", handleConnect);
+    socket.on("disconnect", handleDisconnect);
+
+    return () => {
+      socket.off("connect", handleConnect);
+      socket.off("disconnect", handleDisconnect);
+    };
+  }, []);
+
+  return { socket, isConnected };
+};
