ef115fa2bdde04d8a73a7f2ee38912b815682744
tamir <tamirkhy@gmail.com>
2025-05-23 13:30:24 +0300

    Create main content component

---
+++ apps/client/src/components/MainContent/MainContent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainContent: React.FC = () => {
+  return (
+    <div className="w-full sm:w-2/3 flex items-center justify-center border rounded-lg p-4 h-full sm:h-auto">
+      <p className="text-gray-500">Create New Call</p>
+    </div>
+  );
+};
+
+export default MainContent;
