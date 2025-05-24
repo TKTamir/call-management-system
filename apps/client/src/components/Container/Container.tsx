9e5706a9a678ba0408ed6a881653bbb9f92ae899
tamir <tamirkhy@gmail.com>
2025-05-24 04:13:56 +0300

    Adjust container styling

--- apps/client/src/components/Container/Container.tsx
+++ apps/client/src/components/Container/Container.tsx
@@ -5,7 +5,7 @@ interface PageContainerProps {
 }
 
 const PageContainer: React.FC<PageContainerProps> = ({ children }) => (
-  <div className="flex flex-col border rounded-lg p-6 w-[95%] max-w-7xl mx-auto h-[80dvh]">
+  <div className="mx-auto flex h-[80dvh] w-[95%] max-w-7xl flex-col rounded-xl bg-white p-6 shadow-lg">
     {children}
   </div>
 );
