357624f065b03cc4bdbc90ed020ffc931475a100
tamir <tamirkhy@gmail.com>
2025-05-24 04:18:57 +0300

    Adjust suggestedtaskitem styling

--- apps/client/src/components/SuggestedTask/SuggestedTaskItem.tsx
+++ apps/client/src/components/SuggestedTask/SuggestedTaskItem.tsx
@@ -9,8 +9,8 @@ const SuggestedTaskItem: React.FC<SuggestedTaskItemProps> = ({
   name,
   className,
 }) => (
-  <div className={`flex items-center ${className}`}>
-    <span>{name}</span>
+  <div className={`flex items-center text-gray-700 ${className}`}>
+    <span className="font-medium">{name}</span>
   </div>
 );
 
