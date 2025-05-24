103719a546b98ed282f757f61ab8aeffec33cb2f
tamir <tamirkhy@gmail.com>
2025-05-24 14:55:42 +0300

    Set cors to cloudfront

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -23,12 +23,11 @@ socketService.initialize(server);
 
 app.use(
   cors({
-    origin: true,
+    origin: "https://d39twl30is0v0r.cloudfront.net",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
